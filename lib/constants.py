67f25f9aad3c431f1bdb38f3653be42d3481bf77
Bago <jonas.kris.collier@gmail.com>
2018-05-22 23:17:43 +0200

    Update constants.py

--- lib/constants.py
+++ lib/constants.py
@@ -40,7 +40,7 @@ def read_json(filename, default):
 class BitcoinMainnet:
 
     TESTNET = False
-    WIF_PREFIX = 0xd8
+    WIF_PREFIX = 0x80
     ADDRTYPE_P2PKH = 25
     ADDRTYPE_P2SH = 51
     SEGWIT_HRP = "bery"
